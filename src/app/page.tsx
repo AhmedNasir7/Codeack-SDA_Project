e8dbc54cf7a59fde95ed5d93b46b5d48365eb79e
AhmedNasir7 <ashblazer17@gmail.com>
2025-11-07 00:33:07 +0500

    chore: Template created

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center tracking-tight bg-zinc-50 font-sans dark:bg-black flex-col">
+      <h1 className=" text-3xl font-semibold  text-black dark:text-zinc-50">
+        Codeack: A competitive Programming website to be launched soon!
+      </h1>
+      <p className=" text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+        Having expectations? Wait and see the new modern multiperson
+        competitions!
+      </p>
+    </div>
+  )
+}
